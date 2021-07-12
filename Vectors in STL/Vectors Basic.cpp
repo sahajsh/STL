4c92674c75cc19795fd9bc8c49b02a676b2995b4
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-07-12 20:24:17 +0530

    Create Vectors Basic.cpp

---
+++ Vectors in STL/Vectors Basic.cpp
@@ -0,0 +1,27 @@
+                                        //   <---Vectors in STL---->  \\
+
+
+#include <bits/stdc++.h>
+using namespace std;
+void printVec(vector<int>v){
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+int main() {
+	
+    vector<int>v;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        v.push_back(x);
+
+    }
+  printVec(v);
+
+
+	return 0;
+}
