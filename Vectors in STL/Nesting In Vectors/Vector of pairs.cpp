10e02fe885e1deb7cafa08104b007efff8769789
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-07-13 04:18:20 +0530

    Update Vector of pairs.cpp

--- Vectors in STL/Nesting In Vectors/Vector of pairs.cpp
+++ Vectors in STL/Nesting In Vectors/Vector of pairs.cpp
@@ -15,10 +15,10 @@ void printVector(vector<pair<int,int>>&v){
 
 int main(){
     vector<pair<int,int>>v={{1,2},{2,3},{4,5}};
-    printVector(v);//size: 3
+    printVector(v);    //size: 3
 	               //1  2
-				   //2  3
-				   //4  5   
+		       //2  3
+		       //4  5   
 }
 
 //How to take input of vector of pairs----->
@@ -26,7 +26,7 @@ int main(){
 #include<bits/stdc++.h>
 using namespace std;
 void printVector(vector<pair<int,int>>&v){
-      cout<<"size: "<<v.size()<<endl;
+      cout<<"size: "<<v.size()<<endl;//v.size() has O(1) time complexity
       for(int i=0;i<v.size();i++){
           cout<<v[i].first<<"  "<<v[i].second<<endl;
       }
