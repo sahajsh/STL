3971bbb924762079b2ce16bf924b7914204385dd
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-07-14 03:33:51 +0530

    Update Iterators in STL.cpp

--- Iterators in STL.cpp
+++ Iterators in STL.cpp
@@ -24,23 +24,25 @@ int main(){
 }
 
 /*
+
 // C++ code to demonstrate the working of
 // iterator, begin() and end()
+
 #include<iostream>
 #include<iterator> // for iterators
 #include<vector> // for vectors
 using namespace std;
 int main()
 {
-	vector<int> ar = { 1, 2, 3, 4, 5 };
+	vector<int> v = { 1, 2, 3, 4, 5 };
 	
 	// Declaring iterator to a vector
-	vector<int>::iterator ptr;
+	vector<int>::iterator it;
 	
 	// Displaying vector elements using begin() and end()
 	cout << "The vector elements are : ";
-	for (ptr = ar.begin(); ptr < ar.end(); ptr++)
-		cout << *ptr << " ";
+	for (it = v.begin(); it !=v.end(); it++)
+		cout << *it << " ";
 	
 	return 0;	
 }
