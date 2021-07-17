b4c66f979031e530e1e7dbeb9f6f5cc1197f2fd8
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-07-18 03:32:35 +0530

    Update Queues.cpp

--- Queues.cpp
+++ Queues.cpp
@@ -1,1 +1,22 @@
+//Queues-----------FIFO(first in first out)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	queue<string>q;// creates an empty queue of integer q 
+    q.push("abc");//push() is used to insert the element in the queue.
+    // The element is inserted at the back  of the queue.
+    q.push("efg");
+    q.push("sdf");
+    while(!q.empty()){
+        //front() returns the front element of the queue.
+        cout<<q.front()<<endl;
+        q.pop();//This method removes single element from the front of the queue 
+    }
+
+
+	return 0;
+}
+
 
