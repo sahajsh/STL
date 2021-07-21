cb48298bc0f9aad88262421b924d464d585637d8
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-07-22 02:45:42 +0530

    Update Upper Bound and Lower Bound in C++ STL.cpp

--- Upper Bound and Lower Bound in C++ STL.cpp
+++ Upper Bound and Lower Bound in C++ STL.cpp
@@ -1,4 +1,4 @@
-//Example of Lower bound->
+//Example of lower_bound 
  #include<bits/stdc++.h>
  using namespace std;
  int main(){
@@ -22,3 +22,33 @@
      output- 1 2 3 4 5 7 
              4
 */
+//Example of upper_bound
+ #include<bits/stdc++.h>
+ using namespace std;
+ int main(){
+     int n;
+     cin>>n;
+     int a[n];
+     for(int i=0;i<n;i++){
+         cin>>a[i];
+     }
+     sort(a,a+n);
+     for(int i=0;i<n;i++){
+         cout<<a[i]<<" ";
+     }
+     cout<<endl;
+    
+     //int *ptr=lower_bound(a,a+n,26);
+     int *ptr=upper_bound(a+4,a+n,3);
+     cout<<(*ptr)<<" ";
+ }
+ /* 
+ input - 6 
+        4 3 2 6 1 8
+ 
+ 
+ output -1 2 3 4 6 8 
+        6
+
+    
+  */
