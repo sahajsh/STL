116a32837d5c493e64425fdee41b4a106b202857
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-07-23 02:24:31 +0530

    Update C++ STL Inbuilt Algorithms - Part1.cpp

--- Introduction to algorithms in STL/C++ STL Inbuilt Algorithms - Part1.cpp
+++ Introduction to algorithms in STL/C++ STL Inbuilt Algorithms - Part1.cpp
@@ -2,17 +2,27 @@
 // Algorithms using vectors-----
 
 
-#include<bits/stdc++.h>
+
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int n;
+
+int main() {
+	int n;
     cin>>n;
-    vector<int>v(n);
-    for(int i=0;i<n;i++){
-        cin>>v[i];
-    }
-   int min= *min_element(v.begin(),v.end());
-   cout<<min<<endl;
-   int max= *max_element(v.begin(),v.end());
-   cout<<max<<endl;
+    // Taking initial sum as 0
+	int sum = 0;
+    vector<int> v = {1, 3, 8, 7, 3};
+    int min=*min_element(v.begin()+1,v.end());
+    cout<<" the minimum element present in vector is: "<<min<<endl;
+    int max=*max_element(v.begin(),v.end());
+    cout<<" the maximum element present in vector is: "<<max<<endl;
+    // Demonstration of accumulate function
+	// to sum all the elements of the vector
+	sum = accumulate(v.begin(), v.end(), 0);
+    cout<<" sum of all elements in vector: "<<sum<<endl;
+    int ct=count(v.begin(),v.end(),3);
+    cout << " Number of times 3 appears : "<<ct<<endl;
+
+	
 }
+
