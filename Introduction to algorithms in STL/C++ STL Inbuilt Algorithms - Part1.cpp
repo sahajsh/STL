1e8c0e5f716e2abe1bd3541ab7718b72088af6f9
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-07-23 02:51:11 +0530

    Update C++ STL Inbuilt Algorithms - Part1.cpp

--- Introduction to algorithms in STL/C++ STL Inbuilt Algorithms - Part1.cpp
+++ Introduction to algorithms in STL/C++ STL Inbuilt Algorithms - Part1.cpp
@@ -3,35 +3,43 @@
 
 
 
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	int n;
+    int n;
     cin>>n;
     // Taking initial sum as 0
-	int sum = 0;
-    vector<int> v = {1, 3, 8, 7, 3};
+    int sum = 0;
+    vector<int> v = {3, 3, 8, 7, 3};
     int min=*min_element(v.begin()+1,v.end());
-    cout<<" the minimum element present in vector is: "<<min<<endl;
+    cout<<"the minimum element present in vector is: "<<min<<endl;
     int max=*max_element(v.begin(),v.end());
-    cout<<" the maximum element present in vector is: "<<max<<endl;
+    cout<<"the maximum element present in vector is: "<<max<<endl;
     // Demonstration of accumulate function
-   // to sum all the elements of the vector
+	// to sum all the elements of the vector
     sum = accumulate(v.begin(), v.end(), 0);
-    cout<<" sum of all elements in vector: "<<sum<<endl;
-    int ct=count(v.begin(),v.end(),3);
-    cout << " Number of times 3 appears : "<<ct<<endl;
+    cout<<"sum of all elements in vector: "<<sum<<endl;
+    int ct=count(v.begin()+1,v.end(),3);
+    cout << "Number of times 3 appears : "<<ct<<endl;
+	
     auto it=find(v.begin(),v.end(),3);
     if(it!=v.end()){
-        cout<<" "<<*it<<" is found"<<endl;
+        cout<<*it<<" is found"<<endl;
     }else{
         cout<<"element not found";
     }
+	
    reverse(v.begin(),v.end());
    for(auto value:v){
-       cout<< value<<endl;//it will print reverse vector.
+       cout<<value<<" ";//it will print reverse vector.
    }
    cout<<endl;
-}
+	
+   string s="eurt emoc od smaerd";
+   reverse(s.begin(),s.end());
+   cout<<s<<endl;//
 
+	
+}
