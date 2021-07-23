14d371626044802a91c3d0d70b2659c2f8589a62
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2021-07-23 06:00:36 +0530

    Update Inbuilt STL Algorithms & Lambda Function - Part 2.cpp

--- Introduction to algorithms in STL/Inbuilt STL Algorithms & Lambda Function - Part 2.cpp
+++ Introduction to algorithms in STL/Inbuilt STL Algorithms & Lambda Function - Part 2.cpp
@@ -3,20 +3,11 @@
 
 
 //                                          Lambda Functions example-
-
-/* #include<bits/stdc++.h>
-using namespace std;
-int main(){
-  cout<< [](int x,int y){return (x+y);}(2,8)<<endl;
-
-} */
-
-
-// isko BAAR BAAR use krskty h kitny k liy bhi-
+ 
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-  auto sum =[](int x,int y){return (x+y);};//syntax
+  auto sum =[](int x,int y){return (x+y);};
   cout<<sum(2,3);
 
 }
